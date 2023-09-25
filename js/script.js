e9bf64b406fbf7e77ca61e89ba3526953b87c22e
Anastasiia Deviatkina <anastasia.deviatkina@gmail.com>
2023-09-25 11:29:18 +0200

    Add finding the correct article using the selector (value of 'href' attribute)

--- js/script.js
+++ js/script.js
@@ -21,12 +21,14 @@ const titleClickHandler = function(event){
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
     }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     
     console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
   /* add class 'active' to the correct article */
   
