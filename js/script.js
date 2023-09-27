c09be8e07b1659ca1c330280b535270ec5b971e0
Anastasiia Deviatkina <anastasia.deviatkina@gmail.com>
2023-09-27 18:35:56 +0200

    In section generate list of title were added kod for: remove contents of title list; 'for-of' each article; get article id

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const titleClickHandler = function(event){
     console.log(targetArticle);
 
   /* add class 'active' to the correct article */
-  
+    targetArticle.classList.add('active');
 }
     const links = document.querySelectorAll('.titles a');
   
