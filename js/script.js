6648c3373a0de0e7bffa3affd621cd0f3ba954de
Anastasiia Deviatkina <anastasia.deviatkina@gmail.com>
2023-09-29 10:33:13 +0200

    Add action to clicked tag. Add finding the link to tags

--- js/script.js
+++ js/script.js
@@ -107,13 +107,54 @@ function generateTags(){
             console.log(tagHTML);
             /* add generated code to html variable */
             html += tagHTML;
+        /* END LOOP: for each tag */    
         }
-        /* END LOOP: for each tag */
-        
         /* insert HTML of all the links into the tags wrapper */
         tagsList.innerHTML = html;
         }
     /* END LOOP: for every article: */
-  }
-  
-  generateTags();
+}
+generateTags();
+
+/*4. Dodajemy akcje po kliknięciu w tag*/
+function tagClickHandler(event){
+    /* prevent default action for this event */
+    event.preventDefault();
+    /* make new constant named "clickedElement" and give it the value of "this" */
+    const clickedElement = this;
+    /* make a new constant "href" and read the attribute "href" of the clicked element */
+    const href = clickedElement.getAttribute('href');
+    /* make a new constant "tag" and extract tag from the "href" constant */
+    const tag = document.querySelector(href);
+    /* find all tag links with class active */
+    const activeTags = document.querySelectorAll('.active');
+    /* START LOOP: for each active tag link */
+    for (let activeTag of activeTags){
+        /* remove class active */
+        activeTag.classList.remove('active');
+        /* END LOOP: for each active tag link */
+    }
+    /* find all tag links with "href" attribute equal to the "href" constant */
+    const tagLinks = document.querySelectorAll('a[href="${href}"]');
+    /* START LOOP: for each found tag link */
+    for (let tagLink of tagLinks) {
+        /* add class active */
+        tagLink.classList.add('active');
+    /* END LOOP: for each found tag link */
+    }
+    /* execute function "generateTitleLinks" with article selector as argument */
+    generateTitleLinks();
+}
+
+/*5. Znajdujemy linki do ragów*/
+function addClickListenersToTags(){
+    /* find all links to tags */
+    const tagLinks = document.querySelectorAll('a.active[href^="#tag-"]');
+    /* START LOOP: for each link */
+    for(let tagLink of tagLinks){
+        /* add tagClickHandler as event listener for that link */
+        tagLink.addEventListener('click', tagClickHandler);
+    /* END LOOP: for each link */
+    }
+}  
+addClickListenersToTags();
